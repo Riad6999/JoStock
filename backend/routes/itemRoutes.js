1761a427a0a6801ede7f5f88f05c75ee0fb71dad
Riad6999 <yoyo.in420@gmail.com>
2025-11-10 05:15:48 +0300

    Update itemRoutes.js

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getItems, addItem, updateItem } from "../controllers/itemController.js";
+import { getItems, addItem } from "../controllers/itemController.js";
 
 const router = express.Router();
+
 router.get("/", getItems);
 router.post("/", addItem);
-router.patch("/:id", updateItem);
 
 export default router;
